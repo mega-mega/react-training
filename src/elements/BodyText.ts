4431933087427c76ca9f31caa0d1431880018603
ts <t.shibuya.uva@gmail.com>
2020-02-08 11:50:03 +0900

    edit settings

---
+++ src/elements/BodyText.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+class BodeyText extends React.Component {
+    render() {
+        return {
+            <View>
+            <Text>Hello!</Text>
+            </View>
+        }
+    }
+}
