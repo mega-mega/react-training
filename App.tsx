1f2911eba91845847f313f13018126c290ff2eb4
ts <t.shibuya.uva@gmail.com>
2020-02-09 18:40:28 +0900

    add icon

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MemoListScreen from './src/screens/MemoListScreen';
 import AppBar from './src/components/AppBar';
+import MemoDetailScreean from './src/screens/MemoDetailScreen';
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
       <MemoListScreen />
+      {/* <MemoDetailScreean /> */}
     </View>
   );
 }
--- src/elements/CircleButton.tsx
+++ src/elements/CircleButton.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import fontAwsome from '../../assets/fonts/fa-solid-900.ttf';
 
-class CircleButton extends React.Component {
+class CircleButton extends React.Component<any, {}> {
+  state = {
+    fontLoaded: false,
+  };
+  async componentWillMount() {
+    await Font.loadAsync({
+      FontAwsome: fontAwsome,
+    });
+    this.setState({ fontLoaded: true });
+  }
   render() {
+    const { style, color } = this.props;
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
     return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonTitle}>{this.props.children}</Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        {this.state.fontLoaded ? (
+          <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+            {this.props.children}
+          </Text>
+        ) : null}
       </View>
     );
   }
@@ -18,7 +40,6 @@ const styles = StyleSheet.create({
     right: 32,
     width: 48,
     height: 48,
-    backgroundColor: '#E31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -28,7 +49,8 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
   },
   circleButtonTitle: {
-    fontSize: 32,
+    fontFamily: 'FontAwsome',
+    fontSize: 24,
     lineHeight: 32,
     color: '#fff',
   },
